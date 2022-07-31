fee2e6e8c06d47cee638448259cfc8c04e77b562
Gabriel-Silva-Oliveira <gabriel.silva.oliveira@kyndryl.com>
2022-07-31 19:04:14 -0300

    add tests for authentication controller and cursos repository.

--- src/main/java/br/com/alurarest/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alurarest/forum/config/security/SecurityConfigurations.java
@@ -19,7 +19,7 @@ import br.com.alurarest.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
-@Profile("prod")
+@Profile(value = {"prod", "test"})
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
