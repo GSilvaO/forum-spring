511a64e9735cea00855e577f61153ca36d2900db
Gabriel-Silva-Oliveira <gabriel.silva.oliveira@kyndryl.com>
2022-08-01 18:26:50 -0300

    add mock test for AutenticacaoController. add Dockerfile.

--- src/test/java/br/com/alurarest/forum/controller/AutenticacaoControllerTest.java
+++ src/test/java/br/com/alurarest/forum/controller/AutenticacaoControllerTest.java
@@ -1,7 +1,6 @@
 package br.com.alurarest.forum.controller;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/br/com/alurarest/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alurarest/forum/repository/CursoRepositoryTest.java
@@ -1,19 +1,18 @@
 package br.com.alurarest.forum.repository;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.alurarest.forum.modelo.Curso;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
@@ -35,15 +34,15 @@ public class CursoRepositoryTest {
 		em.persist(html5);
 		
 		Curso curso = cursoRepository.findByNome(nomeCurso);
-		Assert.assertNotNull(curso);
-		Assert.assertEquals(nomeCurso, curso.getNome());
+		Assertions.assertNotNull(curso);
+		Assertions.assertEquals(nomeCurso, curso.getNome());
 	}
 	
 	@Test
 	public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
 		String nomeCurso = "JPA";
 		Curso curso = cursoRepository.findByNome(nomeCurso);
-		Assert.assertNull(curso);
+		Assertions.assertNull(curso);
 	}
 
 }
